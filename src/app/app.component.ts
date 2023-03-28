7f6cc81b65849d1c6f87a0999a6e5e2533a83609
Edgar <edgarmurudu@gmail.com>
2023-03-28 07:43:46 +0200

    recipe model complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(navData: string) {
+    console.log(navData);
+    this.loadedFeature = navData;
+  }
+}
