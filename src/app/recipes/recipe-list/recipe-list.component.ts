c7e21a0922f3cf282983f44bbadff8f03eb329ff
Edgar <edgar.morudu@genasystech.com>
2023-04-19 09:28:07 +0200

    mergeFix

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Test recipe',
-      'Description test',
-      'https://media.istockphoto.com/id/1320857678/photo/brazilian-fish-stew-moqueca.jpg?b=1&s=170667a&w=0&k=20&c=bcE72Zq71JEVt_lfL0fYWMCMjYV4AtxHxxB4EMIZamw='
-    ),
-    new Recipe(
-      'Another recipe',
-      'Description test',
-      'https://thecozycook.com/wp-content/uploads/2022/04/Lasagna-Recipe-f.jpg'
-    ),
-  ];
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService){
+
+  }
+
+  ngOnInit(){
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,21 @@
-export class RecipeService{}
+import { Recipe } from './recipe.model';
+
+
+export class RecipeService{
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'Description test',
+      'https://media.istockphoto.com/id/1320857678/photo/brazilian-fish-stew-moqueca.jpg?b=1&s=170667a&w=0&k=20&c=bcE72Zq71JEVt_lfL0fYWMCMjYV4AtxHxxB4EMIZamw='
+    ),
+    new Recipe(
+      'Test recipe',
+      'Description test',
+      'https://media.istockphoto.com/id/1320857678/photo/brazilian-fish-stew-moqueca.jpg?b=1&s=170667a&w=0&k=20&c=bcE72Zq71JEVt_lfL0fYWMCMjYV4AtxHxxB4EMIZamw='
+    ),
+  ];
+
+  getRecipes(){
+    return this.recipes.slice()
+  }
+}
